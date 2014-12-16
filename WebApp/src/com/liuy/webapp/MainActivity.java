2394c19052ff7e77f3854cef601eb07b11310adc
刘云 <liuy@thinkzheng.com>
2014-12-16 17:27:40 +0800

    添加webview 内容 使用freemaker加载内容

--- WebApp/src/com/liuy/webapp/MainActivity.java
+++ WebApp/src/com/liuy/webapp/MainActivity.java
@@ -1,15 +1,71 @@
 package com.liuy.webapp;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.util.ByteArrayBuffer;
+import org.apache.http.util.EncodingUtils;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
+@SuppressLint("JavascriptInterface")
 public class MainActivity extends Activity {
+	private WebView mWebView;
+	private Handler mHandler = new Handler();
 
-	@Override
+	@SuppressLint("SetJavaScriptEnabled")
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		// setContentView(R.layout.activity_main);
+		setContentView(R.layout.webview);
+		mWebView = (WebView) findViewById(R.id.webview);
+		WebSettings webSettings = mWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		mWebView.addJavascriptInterface(new Contact(), "Contact");
+		// mWebView.loadUrl("file:///android_asset/app/index.html");
+		// mWebView.loadDataWithBaseURL(baseUrl, data, mimeType, encoding,
+		// historyUrl);
+		String data = "";
+		try {
+			// 读取assets目录下的文件需要用到AssetManager对象的Open方法打开文件
+			InputStream is = getAssets().open("app/index.html");
+			// loadData()方法需要的是一个字符串数据所以我们需要把文件转成字符串
+			ByteArrayBuffer baf = new ByteArrayBuffer(500);
+			int count = 0;
+			while ((count = is.read()) != -1) {
+				baf.append(count);
+			}
+			data = EncodingUtils.getString(baf.toByteArray(), "utf-8");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		mWebView.loadData(data, "text/html", "utf-8");
+	}
+
+	private final class Contact {
+		// JavaScript调用此方法拨打电话
+		public void call(String phone) {
+			mWebView.loadUrl("javascript:wave()");
+		}
+
+		// Html调用此方法传递数据Android4.2及以后，注入步骤如下：
+		@JavascriptInterface
+		public void showcontacts() {
+			Log.i("MainActivity", "调用js方法");
+			mHandler.post(new Runnable() {
+				public void run() {
+					mWebView.loadUrl("javascript:wave()");
+				}
+			});
+		}
 	}
 
 	@Override
